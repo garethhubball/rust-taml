c1b8eae6f05202921cb305cf9762ef2d3de5ab10
Gareth Hubball <ridecar2@gmail.com>
2020-09-20 23:28:52 +0100

    Start work on more recursion

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,37 @@
 #[derive(PartialEq, Debug)]
 pub enum TItem<'a> {
     KeyValuePair(TKeyValuePair<'a>),
-    String(&'a str)
+    String(&'a str),
+    Array(TArray<'a>)
 }
 
 #[derive(PartialEq, Debug)]
 pub struct TKeyValuePair<'a> {
     key: &'a str,
-    value: &'a str
+    value: Box<TItem<'a>>
 }
 
 #[derive(PartialEq, Debug)]
-pub struct TArray<'a>(Vec<TItem<'a>>);
+pub struct TArray<'a>(Vec<Box<TItem<'a>>>);
 
 pub fn parse(document: &str) -> TArray {
     parse_array(document)
 }
 
 fn parse_array(fragment: &str) -> TArray {
-    let items: Vec<TItem> = fragment.lines().map(|item| {
+    let items: Vec<Box<TItem>> = fragment.lines().map(|item| {
         if item.contains('\t') {
-            TItem::KeyValuePair(parse_kvp(item))
+            Box::from(TItem::KeyValuePair(parse_kvp(item)))
         } else {
-            TItem::String(item)
+            Box::from(TItem::String(item))
         }
     }).collect();
     TArray(items)
 }
 
-fn parse_kvp(line: &str) -> TKeyValuePair {
-    let kvp: Vec<&str> = line.split('\t').filter(|item| item.len() > 0).collect();
-    TKeyValuePair { key: kvp[0], value: kvp[1] }
+fn parse_kvp(fragment: &str) -> TKeyValuePair {
+    let kvp: Vec<&str> = fragment.split('\t').filter(|item| item.len() > 0).collect();
+    TKeyValuePair { key: kvp[0], value: Box::from(TItem::String(kvp[1])) }
 }
 
 #[cfg(test)]
@@ -40,29 +41,44 @@ mod tests {
     #[test]
     fn simple_key_value_pair() {
         let document = "key\tvalue";
-        let expected = TKeyValuePair { key: "key", value: "value" };
+        let expected = TKeyValuePair { key: "key", value: Box::from(TItem::String("value")) };
         assert_eq!(expected, parse_kvp(document));
     }
 
     #[test]
     fn simple_key_value_pair_with_multiple_tabs() {
         let document = "key\t\t\tvalue";
-        let expected = TKeyValuePair { key: "key", value: "value" };
+        let expected = TKeyValuePair { key: "key", value: Box::from(TItem::String("value")) };
         assert_eq!(expected, parse_kvp(document));
     }
 
     #[test]
     fn simple_array() {
         let document = "item1\nitem2";
-        let expected = TArray(vec![TItem::String("item1"), TItem::String("item2")]);
+        let expected = TArray(vec![Box::from(TItem::String("item1")), Box::from(TItem::String("item2"))]);
         assert_eq!(expected, parse_array(document));
     }
 
     #[test]
     fn array_of_kvp() {
         let document = "key\tvalue\nkey2\tvalue2";
-        let expected = TArray(vec![TItem::KeyValuePair(TKeyValuePair {key: "key", value: "value"}),
-                                   TItem::KeyValuePair(TKeyValuePair {key: "key2", value: "value2"})]);
+        let expected = TArray(vec![Box::from(TItem::KeyValuePair(TKeyValuePair {key: "key", value: Box::from(TItem::String("value"))})),
+                                   Box::from(TItem::KeyValuePair(TKeyValuePair {key: "key2", value: Box::from(TItem::String("value2"))}))]);
         assert_eq!(expected, parse_array(document));
+    }
+
+    #[test]
+    #[ignore]
+    /// TODO: Does not yet work and requires significant work to fix.
+    fn kvp_with_array_value() {
+        let document = "fruits\n\tapple\n\tbanana";
+        let expected = TKeyValuePair {
+            key: "fruits",
+            value: Box::from(TItem::Array(
+                TArray(
+                    vec![
+                        Box::from(TItem::String("apple")),
+                        Box::from(TItem::String("banana"))]))) };
+        assert_eq!(expected, parse_kvp(document));
     }
 }
